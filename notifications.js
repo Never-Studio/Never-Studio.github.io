dfe093a0ffcb7a1f867c9030def889a396d449a0
Nils <120112184+Nils-nonline@users.noreply.github.com>
2024-09-11 17:31:28 +0200

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -24,19 +24,10 @@ const observer = new IntersectionObserver((entries) => {
 
 sections.forEach(section => observer.observe(section));
 
-var notifications = [["New project launched!","We launched a new project! <br> How cool!"],
-					["New project launched!","We launched a new project!"],
-					["New project launched!","We launched a new project!"],
-					["New project launched!","We launched a new project!"],
-					["New project launched!","We launched a new project! <br> How cool!"],
-					["New project launched!","We launched a new project!"],
-					["New project launched!","We launched a new project!"],
-					["New project launched!","We launched a new project!"],
-					["New project launched!","We launched a new project! <br> How cool!"],
-					["New project launched!","We launched a new project!"],
-					["New project launched!","We launched a new project!"],
-					["New project launched!","We launched a new project!"]
-					]
+var notifications = [["Planned: Never World","An infinite ai generated world."],
+		     ["Working on: Never RPG","A pixel rpg with currently limited access."],
+		     ["Working on: Never Isometric Engine","The engine behind Never RPG. <br> It will be public and free to use. <br> Coming soon!"]
+		   ]
 
 // Adjust viewport height for mobile devices
 function adjustViewportHeight() {
