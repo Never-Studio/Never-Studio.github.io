fb0eea862ee878df397d2b3ae7cf8f3c7ce5cb6a
MHausOnline <118628201+MHausOnline@users.noreply.github.com>
2024-12-31 15:30:48 +0100

    Update notifications-list.js

--- notifications-list.js
+++ notifications-list.js
@@ -1,4 +1,4 @@
-var notifications = [["Working on: Never Isometric Engine","The engine behind Never RPG. <br> It will be public and free to use. <br> Coming soon!"],
+var notifications = [["Working on: Never Isometric Engine","The beta is already available. <br> It will be public and free to use. <br> Coming soon!"],
 		     ["Planned: Never World","An infinite ai generated world."],
 		     ["Working on: Never RPG","A pixel rpg with currently limited access."]
 		   ]
