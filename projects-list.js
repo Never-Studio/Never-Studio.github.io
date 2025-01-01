9a50daea657233baa8a03f3118d7e606050f25e3
MHausOnline <118628201+MHausOnline@users.noreply.github.com>
2025-01-01 12:28:08 +0100

    Update projects-list.js

--- projects-list.js
+++ projects-list.js
@@ -1,3 +1,3 @@
-var projects = [["Never Isometric Engine","WE are working on it...","neverengine.png","https://www.neverstudio.de"],
-		["Never RPG 1","Still under development ...","neverRPG.png","https://www.neverstudio.de"]
+var projects = [["Never Isometric Engine","We are working on it...","neverengine.png","https://www.neverstudio.de","an image of a pixel figure in front of the text 'hello'"],
+		["Never RPG 1","Still under development ...","neverRPG.png","https://www.neverstudio.de","the logo of neverstudio: a stylized hourglass lying on its side"]
 		]
