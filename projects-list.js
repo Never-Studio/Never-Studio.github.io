76d76bcead9775ae59f42b438edd23b6e4bd81fd
MHausOnline <118628201+MHausOnline@users.noreply.github.com>
2025-05-28 19:14:17 +0200

    SEO
    Improved SEO

--- projects-list.js
+++
@@ -1,3 +0,0 @@
-var projects = [["Never Isometric Engine","We are working on it...","neverengine.png","https://www.neverstudio.de","an image of a pixel figure in front of the text 'hello'"],
-		["Never RPG 1","Still under development ...","neverRPG.png","https://www.neverstudio.de","the logo of neverstudio: a stylized hourglass lying on its side"]
-		]
