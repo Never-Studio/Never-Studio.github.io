735a3444cb6d730f9a24e6290b872f3fd9761cff
MHausOnline <118628201+MHausOnline@users.noreply.github.com>
2025-08-28 00:37:26 +0200

    Moved language switching script into seperate js file

---
+++ langswitch.js
@@ -0,0 +1,12 @@
+function switchToGerman() {
+  const path = window.location.pathname;
+  if (!path.startsWith('/de')) {
+	window.location.href = '/de' + path;
+  }
+}
+function switchToEnglish() {
+  const path = window.location.pathname;
+  if (path.startsWith('/de')) {
+	window.location.href = path.replace("/de","");
+  }
+}
