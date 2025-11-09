603b583edf193764e4058b8b988b1028a2185bf9
Nils-nonline <nils-nonline@web.de>
2025-11-09 19:55:33 +0100

    accessibility code

--- colorscript.js
+++ colorscript.js
@@ -1,3 +1,18 @@
 let bcolors = ["#E34234","#4d673a","#5F92EC","#ff8c00"];
+
+if(window.matchMedia('(forced-colors: active)').matches){
+    bcolors = ["#EC6F65", "#77A262", "#6396EE", "#FF8C00"];
+
+    var all = document.getElementsByTagName("*");
+    
+    for (var i=0, max=all.length; i < max; i++) {
+        all[i].style.color = "black";
+    }
+}
+
 var backgroundC = bcolors[Math.floor(Math.random()*bcolors.length)];
 document.body.style.backgroundColor = backgroundC;
+
+
+//white #AC2216 #485F35 #1553BC #8A4600
+//black #EC6F65 #77A262 #6396EE #FF8C00
